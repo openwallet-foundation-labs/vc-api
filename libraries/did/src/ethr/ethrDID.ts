baee1cde5c5c1ac707104a6530e3163ccd56f0d1
John Henderson <john.henderson@energyweb.org>
2021-11-07 11:25:07 +0100

    docs(did::ethrDID): improve class documentation

--- libraries/did/src/ethr/ethrDID.ts
+++ libraries/did/src/ethr/ethrDID.ts
@@ -1,6 +1,11 @@
 import { Entity, Column } from 'typeorm';
 
-//TODO: Remove definite assignment assertion (!) from properties
+/**
+ * An entity representing a DID for the "ethr" DID method
+ * It has typeorm annotations so that it can be used directly in apps
+ * https://github.com/typeorm/typeorm/blob/master/docs/entities.md
+ * TODO: Remove definite assignment assertion (!) from properties. Need to check if still works if using constructor
+ */
 @Entity()
 export class EthrDID {
   @Column('text', { primary: true })
