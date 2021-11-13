db987d7f80dd469dc46847e528a6bd336bfdb85e
John Henderson <john.henderson@energyweb.org>
2021-11-13 09:53:23 +0100

    feat(kms-interface): add IPrivateKeyFromThumprint

--- libraries/kms-interface/src/index.ts
+++ libraries/kms-interface/src/index.ts
@@ -1,3 +1,5 @@
 export * from './generation/secp256k1-key-gen';
 export * from './generation/ed25519-key-gen';
 export * from './generation/key-gen-result';
+
+export * from './retrieval/private-key-from-thumbprint';
