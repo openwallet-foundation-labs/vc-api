9aa040984a78a10a667e3e2a104d935cb8773ef9
John Henderson <john.henderson@energyweb.org>
2021-11-14 08:56:08 +0100

    test(nest-api): remove duplication from e2e

--- apps/nestjs-wallet/test/app.e2e-spec.ts
+++ apps/nestjs-wallet/test/app.e2e-spec.ts
@@ -17,37 +17,28 @@ describe('App (e2e)', () => {
 
   describe('DID', () => {
     it('should create and retrieve an ethr DID', async () => {
-      const postResponse = await request(app.getHttpServer())
-        .post('/did')
-        .send({ method: 'ethr' })
-        .expect(201);
-      expect(postResponse.body).toHaveProperty('id');
-      expect(postResponse.body).toHaveProperty('verificationMethod');
-      expect(postResponse.body['verificationMethod']).toHaveLength(1);
-      const newDID = postResponse.body.id;
-      const method = newDID.split(':')[1];
-      expect(method).toEqual('ethr');
-
-      const getResponse = await request(app.getHttpServer()).get(`/did/${newDID}`).expect(200);
-      expect(getResponse.body).toHaveProperty('verificationMethod');
-      expect(postResponse.body['verificationMethod']).toMatchObject(getResponse.body['verificationMethod']);
+      await createDID('ethr');
     });
 
     it('should create and retrieve an key DID', async () => {
+      await createDID('key');
+    });
+
+    async function createDID(requestedMethod: string) {
       const postResponse = await request(app.getHttpServer())
         .post('/did')
-        .send({ method: 'key' })
+        .send({ method: requestedMethod })
         .expect(201);
       expect(postResponse.body).toHaveProperty('id');
       expect(postResponse.body).toHaveProperty('verificationMethod');
       expect(postResponse.body['verificationMethod']).toHaveLength(1);
       const newDID = postResponse.body.id;
-      const method = newDID.split(':')[1];
-      expect(method).toEqual('key');
+      const createdMethod = newDID.split(':')[1];
+      expect(createdMethod).toEqual(requestedMethod);
 
       const getResponse = await request(app.getHttpServer()).get(`/did/${newDID}`).expect(200);
       expect(getResponse.body).toHaveProperty('verificationMethod');
       expect(postResponse.body['verificationMethod']).toMatchObject(getResponse.body['verificationMethod']);
-    });
+    }
   });
 });
