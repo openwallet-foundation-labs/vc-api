671a0c59d965b07503de9d741092df7379e24142
John Henderson <john.henderson@energyweb.org>
2021-11-16 17:35:06 +0100

    test(nest-api::creds): more detail to issue unit test

--- apps/nestjs-wallet/src/credentials/credentials.service.spec.ts
+++ apps/nestjs-wallet/src/credentials/credentials.service.spec.ts
@@ -19,10 +19,10 @@ describe('CredentialsService', () => {
   });
 
   it('should be able to issue a credential', async () => {
-    const key = generateEd25519Key();
+    const key =
+      '{"kty":"OKP","crv":"Ed25519","x":"gZbb93kdEoQ9Be78z7NG064wBq8Vv_0zR-qglxkiJ-g","d":"XXugDYUEtINLUefOLeztqOOtPukVIvNPreMTzl6wKgA"}';
     const did = keyToDID('key', key);
     const verificationMethod = await keyToVerificationMethod('key', key);
-    // "did:key:z6MktsT5A2ZpyM21x4GPHiejjrFLejZeLnNeFxCq3cuQdiK3#z6MktsT5A2ZpyM21x4GPHiejjrFLejZeLnNeFxCq3cuQdiK3"
     const credential = {
       '@context': ['https://www.w3.org/2018/credentials/v1'],
       id: 'http://example.org/credentials/3731',
@@ -35,10 +35,34 @@ describe('CredentialsService', () => {
     };
     const proofOptions = {
       proofPurpose: 'assertionMethod',
-      verificationMethod: verificationMethod
+      verificationMethod: verificationMethod,
+      created: '2021-11-16T14:52:19.514Z'
     };
     const jsonWebKey: JWK = JSON.parse(key);
-    const vc = await service.issueCredential(credential, proofOptions, jsonWebKey);
-    expect(vc).toBeDefined();
+    const vc = JSON.parse(await service.issueCredential(credential, proofOptions, jsonWebKey));
+    const expectedVc = {
+      '@context': ['https://www.w3.org/2018/credentials/v1'],
+      id: 'http://example.org/credentials/3731',
+      type: ['VerifiableCredential'],
+      credentialSubject: { id: 'did:example:d23dd687a7dc6787646f2eb98d0' },
+      issuer: 'did:key:z6MkoB84PJkXzFpbqtfYV5WqBKHCSDf7A1SeepwzvE36QvCF',
+      issuanceDate: '2020-08-19T21:41:50Z',
+      proof: {
+        type: 'Ed25519Signature2018',
+        proofPurpose: 'assertionMethod',
+        verificationMethod:
+          'did:key:z6MkoB84PJkXzFpbqtfYV5WqBKHCSDf7A1SeepwzvE36QvCF#z6MkoB84PJkXzFpbqtfYV5WqBKHCSDf7A1SeepwzvE36QvCF',
+        created: '2021-11-16T14:52:19.514Z',
+        jws: 'eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..9N2qqOBBcQkJv_tF1DObaVRovT8nbuDLV1VMFk5sEd_WNKCcdGsPzNoOqVAJI7rCmzgqCtN_dZtzKrtnPZioDg'
+      }
+    };
+    expect(vc['proof']['jws']).toBeDefined();
+    /**
+     * Delete jws from proof as it is not deterministic
+     * TODO: confirm this from the Ed25519Signature2018 spec
+     */
+    delete vc.proof.jws;
+    delete expectedVc.proof.jws;
+    expect(vc).toEqual(expectedVc);
   });
 });
