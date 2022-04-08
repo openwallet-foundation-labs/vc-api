6d33b428cbdce78b5e5fc9400134cf7672a761a7
John Henderson <john.henderson@energyweb.org>
2022-04-08 16:40:47 +0200

    chore(verification-result): remove unused import

--- apps/nestjs-wallet/src/vc-api/credentials/types/verification-result.ts
+++ apps/nestjs-wallet/src/vc-api/credentials/types/verification-result.ts
@@ -1,5 +1,3 @@
-import { IsString, IsArray } from 'class-validator';
-
 /**
  * A response object from verification of a credential or a presentation.
  * https://w3c-ccg.github.io/vc-api/verifier.html
