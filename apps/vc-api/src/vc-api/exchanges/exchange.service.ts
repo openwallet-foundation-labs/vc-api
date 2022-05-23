50984cedd82a3c536d763da1c65404b166f6231b
John Henderson <john.henderson@energyweb.org>
2022-05-23 12:03:59 +0200

    fix(exchaing.service): stringify result before logging

--- apps/vc-api/src/vc-api/exchanges/exchange.service.ts
+++ apps/vc-api/src/vc-api/exchanges/exchange.service.ts
@@ -104,7 +104,7 @@ export class ExchangeService {
       // TODO: check if toDto is working. Seems be keeping it as Entity type.
       const body = TransactionDto.toDto(transaction);
       this.httpService.post(callback.url, body).subscribe({
-        next: (v) => Logger.log(v),
+        next: (v) => Logger.log(JSON.stringify(v)),
         error: (e) => Logger.error(e)
       });
     });
